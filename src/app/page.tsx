bead409edd4f8a713d89cc88d7a39ac0e78efa30
Pedro Cardoso da silva <pcsilvamaster@gmail.com>
2024-02-01 16:56:07 -0300

    theme configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { useTheme } from 'next-themes'
+
 export default function Home() {
+  const { theme, setTheme } = useTheme()
+
   return (
     <div>
       <h1 className="font-bold text-red-400">teste</h1>
+      <div>
+        <button onClick={() => setTheme('light')}>Light</button>
+        <br/>
+        <button onClick={() => setTheme('dark')}>Dark</button>
+    </div>
     </div>
   )
 }
