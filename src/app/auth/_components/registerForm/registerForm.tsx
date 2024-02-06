6ab28df26cc1634b79de6bf3177d3fd312e96e6b
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-02-06 15:53:14 -0300

    feat: pattern component input

--- src/app/auth/_components/registerForm/registerForm.tsx
+++ src/app/auth/_components/registerForm/registerForm.tsx
@@ -1,12 +1,54 @@
-import { Input } from "@/components/input"
+"use client"
+
+import {
+    Input,
+    InputContainer,
+    InputContent,
+    InputRoot,
+} from "@/components/input"
 import { Bell } from "@/icons/bell"
-import { Sun } from "@/icons/sun"
+import { Eye } from "@/icons/eye"
+import { EyeSlash } from "@/icons/eyeSlash"
+import { SyntheticEvent, useState } from "react"
 
 export const RegisterForm = () => {
+    const [show, setShow] = useState(true)
+
+    function handleClick(e: SyntheticEvent) {
+        e.preventDefault()
+        setShow(!show)
+    }
+
     return (
         <form className="flex flex-col gap-3">
-            <Input placeholder="Email" />
-            <Input placeholder="Senha" />
+            <InputRoot>
+                <InputContainer>
+                    <InputContent>
+                        <Bell width={16} height={16} />
+                    </InputContent>
+                    <Input placeholder="Email" />
+                </InputContainer>
+            </InputRoot>
+            <InputRoot>
+                <InputContainer>
+                    <Input
+                        type={`${show ? "password" : "text"}`}
+                        placeholder="Senha"
+                    />
+                    <InputContent>
+                        <button
+                            className="flex items-center justify-center"
+                            onClick={handleClick}
+                        >
+                            {show ? (
+                                <Eye width={22} height={22} />
+                            ) : (
+                                <EyeSlash width={22} height={22} />
+                            )}
+                        </button>
+                    </InputContent>
+                </InputContainer>
+            </InputRoot>
         </form>
     )
 }
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,22 +1,55 @@
-import { InputHTMLAttributes } from "react"
+import { HTMLAttributes, InputHTMLAttributes } from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 import React from "react"
 import { cn } from "@/lib/utils"
 
-interface InputProps
-    extends InputHTMLAttributes<HTMLInputElement>,
-        VariantProps<typeof inputVariants> {
-    startContent?: React.ReactNode
-    endContent?: React.ReactNode
+/* __________________________root_________________________*/
+interface InputRootProps
+    extends HTMLAttributes<HTMLDivElement>,
+        VariantProps<typeof inputRootVariants> {
+    children?: React.ReactNode
     error?: string
 }
 
-const inputVariants = cva(
-    "p-2 border rounded-xl outline-1 focus-within:outline",
+const inputRootVariants = cva("flex flex-col items-start gap-1", {
+    variants: {
+        variant: {
+            default: "",
+        },
+    },
+    defaultVariants: {
+        variant: "default",
+    },
+})
+
+export const InputRoot = React.forwardRef<HTMLDivElement, InputRootProps>(
+    ({ variant, className, error, children, ...props }, ref) => {
+        return (
+            <div
+                {...props}
+                ref={ref}
+                className={cn(inputRootVariants({ variant, className }))}
+            >
+                {children}
+                {error && <p className="text-sm text-red-500">{error}</p>}
+            </div>
+        )
+    }
+)
+
+/* __________________________container_________________________*/
+interface InputContainerProps
+    extends HTMLAttributes<HTMLDivElement>,
+        VariantProps<typeof inputContainerVariants> {
+    children?: React.ReactNode
+}
+
+const inputContainerVariants = cva(
+    "flex items-center justify-center rounded-xl border p-2 outline-1 focus-within:outline",
     {
         variants: {
             variant: {
-                default: "bg-input",
+                default: "",
                 error: "border-red-500 outline-red-500",
             },
         },
@@ -26,29 +59,72 @@ const inputVariants = cva(
     }
 )
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-    (
-        { variant, className, error, startContent, endContent, ...props },
-        ref
-    ) => {
+export const InputContainer = React.forwardRef<
+    HTMLDivElement,
+    InputContainerProps
+>(({ variant, className, children, ...props }, ref) => {
+    return (
+        <div className={cn(inputContainerVariants({ variant, className }))}>
+            {children}
+        </div>
+    )
+})
+
+/* __________________________content_________________________*/
+interface InputContentProps
+    extends HTMLAttributes<HTMLDivElement>,
+        VariantProps<typeof inputContentVariants> {
+    children?: React.ReactNode
+}
+
+const inputContentVariants = cva("", {
+    variants: {
+        variant: {
+            default: "",
+        },
+    },
+    defaultVariants: {
+        variant: "default",
+    },
+})
+
+export const InputContent = React.forwardRef<HTMLDivElement, InputContentProps>(
+    ({ variant, className, children, ...props }, ref) => {
         return (
-            <div className="flex flex-col items-start gap-1">
-                <div
-                    className={cn(
-                        `flex items-center justify-center`,
-                        inputVariants({ variant, className })
-                    )}
-                >
-                    {startContent}
-                    <input
-                        {...props}
-                        className="mx-1 bg-transparent focus:outline-none"
-                        ref={ref}
-                    />
-                    {endContent}
-                </div>
-                {error && <p className="text-sm text-red-500">{error}</p>}
+            <div
+                {...props}
+                className={cn(inputContentVariants({ variant, className }))}
+            >
+                {children}
             </div>
+        )
+    }
+)
+
+/* __________________________input_________________________*/
+interface InputProps
+    extends InputHTMLAttributes<HTMLInputElement>,
+        VariantProps<typeof inputVariants> {}
+
+const inputVariants = cva("mx-1 bg-transparent focus:outline-none", {
+    variants: {
+        variant: {
+            default: "",
+        },
+    },
+    defaultVariants: {
+        variant: "default",
+    },
+})
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+    ({ variant, className, ...props }, ref) => {
+        return (
+            <input
+                ref={ref}
+                {...props}
+                className={cn(inputVariants({ className, variant }))}
+            />
         )
     }
 )
